d0b7299ed90ab38b27829b49729f4bb3285af037
Dexter Chan <dexterchann@gmail.com>
2020-06-06 13:40:28 -0700

    add smooth scroll for anchor links

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 $(document).ready(() => {
+	$('.topnav .icon').on('click', () => {
+		let navElement = document.getElementById('nav');
+		if (navElement.className === 'topnav') {
+			navElement.className += ' responsive';
+			$('.nav-right').slideDown();
+		} else {
+			$('.nav-right').slideUp(() => {
+				console.log('nav closed');
 
-  $(".topnav .icon").on('click', () => {
-    let navElement = document.getElementById("nav");
-    if (navElement.className === "topnav") {
-      navElement.className += " responsive";
-      $(".nav-right").slideDown();
-    } else {
-      $(".nav-right").slideUp(() => {
-        console.log("nav closed");
+				navElement.className = 'topnav';
+			});
+		}
+	});
 
-        navElement.className = "topnav";
-      });
-    }
-  })
+	document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+		anchor.addEventListener('click', function(e) {
+			e.preventDefault();
+
+			document.querySelector(this.getAttribute('href')).scrollIntoView({
+				behavior: 'smooth'
+			});
+		});
+	});
 });
-
